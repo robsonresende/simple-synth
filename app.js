8b0d9b0ae66da4aedeb961a299077afcf3b9914a
Robson Resende <rresende1975@gmail.com>
2024-11-16 13:22:16 -0300

    Synthesizer synchronicity is ready

--- app.js
+++ app.js
@@ -4,43 +4,37 @@
 
 // Detune 
 
-function play(x, wav) {
+const keys = document.querySelectorAll(".pianokey")
+
+keys.forEach((key) => {
+      key.addEventListener('pointerdown', (e) => {
+
+        // pointerup to off the button
+      key.addEventListener('pointerup', (e) => {
+        osc.stop();
+      });
+
+      // pointerleave in case the user leave the button
+      key.addEventListener('pointerleave', (e) => {
+        osc.stop();
+      });
     
-    const ctx = new (window.AudioContext || window.webkitAudioContext)();
+      const ctx = new (window.AudioContext || window.webkitAudioContext)();
 
-    const master = ctx.createGain();
-    master.gain.value = 0.9;
-    master.connect(ctx.destination);
+      const master = ctx.createGain();
+      master.gain.value = 0.9;
+      master.connect(ctx.destination);
 
-    const osc = ctx.createOscillator();
-    osc.frequency.value = wav;
+      const osc = ctx.createOscillator();
+      osc.frequency.value = key.dataset.key;
 
-    osc.type = "triangle";
+      osc.type = "triangle";
 
-    osc.connect(master);
+      osc.connect(master);
 
-    // Access the button
-    const mybutton = document.getElementById(x);
-
-
-    // pointerdown to on the button
-    mybutton.addEventListener('pointerdown', () => {
       osc.start();
     });
 
-    // pointerup to off the button
-    mybutton.addEventListener('pointerup', () => {
-      osc.stop();
-      mybutton.blur();
-      return 0;
-    });
-
-    // pointerleave in case the user leave the button
-    mybutton.addEventListener('pointerleave', () => {
-      osc.stop();
-      mybutton.blur();
-      return 0;
-    });
-
-}
+    
+});
 
